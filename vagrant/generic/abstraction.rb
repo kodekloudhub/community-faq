78112e64aada59f8802ab63c9c3c96f42afc6dbe
Alistair Mackay <34012094+fireflycons@users.noreply.github.com>
2024-07-27 15:00:11 +0100

    RAM check

--- vagrant/generic/abstraction.rb
+++ vagrant/generic/abstraction.rb
@@ -445,3 +445,74 @@ class ArmLinuxHost < LinuxHost
     return "ARM"
   end
 end
+
+def b_puts(str, width = 71)
+  padding = width - 4 - str.length
+  if padding < 1
+    padding = 1
+  end
+  pad = " " * padding
+  puts "# #{str}#{pad} #"
+end
+
+def show_system_info(trigger, host)
+  begin
+    puts "#######################################################################"
+    b_puts("")
+    b_puts("If raising a question on our forums, please PASTE (not screenshot)")
+    b_puts("the content of this box with your question")
+    b_puts("")
+    b_puts("Detecting your hardware...")
+    b_puts "- System: #{host.os_name()}"
+    b_puts("- CPU:    #{host.cpu_name()} (#{host.cpu_count()} cores)")
+    b_puts("- RAM:    #{host.physical_ram_gb()} GB")
+    if !host.hypervisor_exists?
+      raise DetectionError.new "FATAL - Missing #{host.hypervisor_name()}. Please install it first."
+    end
+  rescue DetectionError => e
+    b_puts("#{e}")
+    trigger.abort = true
+    raise
+  ensure
+    b_puts("")
+    puts "#######################################################################\n\n"
+  end
+end
+
+def validate_configuration(trigger, host, vms)
+  requested_ram = 0
+  vms.each do |vm|
+    requested_ram += vm[:memory]
+  end
+  if (requested_ram / 1024) > host.physical_ram_gb - 2
+    puts <<~EOT
+           Total RAM requested by VMs is more than your machine can provide!
+           Either reduce the memory requirement of the VMs or have fewer VMs
+
+         EOT
+    trigger.abort = true
+    raise Exception.new "Invalid configuration. See messages above\n\n"
+  end
+end
+
+def post_provision(env)
+  # Build hosts file fragment
+  puts "--> Harvesting machine IPs"
+  hosts = ""
+  env.active_machines.each do |active_machine|
+    vm_name = active_machine[0].to_s
+    ip = %x{ vagrant ssh -c primary-ip #{vm_name} }
+    hosts << ip << " " << vm_name << "\n"
+  end
+  # Adjust hosts file
+  File.open("hosts.tmp", "w") { |file| file.write(hosts) }
+  env.active_machines.each do |active_machine|
+    vm_name = active_machine[0].to_s
+    puts "--> Setting hosts file: #{vm_name}"
+    %x{ vagrant upload hosts.tmp /tmp/hosts.tmp #{vm_name}}
+    %x{ vagrant ssh -c 'sudo sed -i "/#{vm_name}/d" /etc/hosts ; cat /tmp/hosts.tmp | sudo tee -a /etc/hosts' #{vm_name}}
+  end
+  puts ""
+  puts hosts
+  File.delete("hosts.tmp")
+end
