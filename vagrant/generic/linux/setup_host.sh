47f517915bfd028fa9c129213b48ed2fe16ae5a4
Alistair Mackay <34012094+fireflycons@users.noreply.github.com>
2024-07-29 07:21:56 +0100

    Handle multiple IF with same IP (WMware)

--- vagrant/generic/linux/setup_host.sh
+++ vagrant/generic/linux/setup_host.sh
@@ -23,7 +23,8 @@ then
 else
     # Determine machine IP from route table -
     # Interface that is connected to the NAT network.
-    MY_IP="$(ip route | awk '/'${IP_NW}'/ { print $9 }')"
+    # VMware may create 2 entries with different metrics
+    MY_IP="$(ip route | awk '/'${IP_NW}'/ { print $9 }') | uniq"
     MY_NETWORK=$IP_NW
 fi
 
